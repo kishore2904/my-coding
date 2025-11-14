5e51eeab5ac7648b0e323c1e58b73b92ca9af950
unfold <studiovogue01@gmail.com>
2025-11-14 22:35:24 +0530

    Java 8 interview questions

--- src/Java8/Java8InterviewQuestionsStream.java
+++ src/Java8/Java8InterviewQuestionsStream.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Java8InterviewQuestions {
+public class Java8InterviewQuestionsStream {
 
     public static void main(String[] args) {
 
