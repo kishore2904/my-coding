75038cc71e9d92dd4ddf4ae944c419a5a1b8e5e5
unfold <studiovogue01@gmail.com>
2025-11-14 22:34:54 +0530

    Java 8 interview questions

--- src/Java8/Java8InterviewQuestions.java
+++ src/Java8/Java8InterviewQuestions.java
@@ -24,7 +24,7 @@ public class Java8InterviewQuestions {
                 .stream()
                 .filter(x -> x.getValue() > 1).map(Map.Entry::getKey).toList();
 
-        // To get the unique element from string
+        // 3. To get the unique element from string
         String uniqueElement = "kishorekannanjithu";
         List<String> uniqueList = Arrays.stream(uniqueElement.split(""))
                 .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
@@ -33,7 +33,7 @@ public class Java8InterviewQuestions {
                 .map(Map.Entry::getKey).toList();
         System.out.println(list);
 
-        // To get first non-repeating char
+        // 4. To get first non-repeating char
         String nonRepeatingElement = "kishorekannanjithu";
         String nonRepeating = Arrays.stream(nonRepeatingElement.split(""))
                 .collect(Collectors.groupingBy(x -> x, LinkedHashMap::new, Collectors.counting()))
@@ -42,6 +42,26 @@ public class Java8InterviewQuestions {
                 .filter(x -> x.getValue() == 1)
                 .findFirst().get().getKey();
         System.out.println(nonRepeating);
+
+        // 5. Second-Highest number from array
+
+        int[] numbers = {5,9,11,2,8,21,1};
+        Integer secondHighest = Arrays.stream(numbers).boxed().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
+        System.out.println("Second Highest number: "+secondHighest);
+
+        // 6. Find the longest string in given array
+
+        String[] arrayList = {"Java", "Springboot","Python"};
+
+        String s = Arrays.stream(arrayList).reduce((word1, word2) -> word1.length() > word2.length() ? word1 : word2).get();
+        System.out.println("Longest String: "+ s);
+
+        // 7. Find the values that start with 1
+
+        List<Integer> integerList = List.of(1,5,6,7,11,21,16,111);
+        List<String> valueStartsWith1 = integerList.stream().map(x -> x.toString()).filter(x -> x.startsWith("1")).toList();
+        System.out.println("Value starts with 1: "+valueStartsWith1);
+
 
     }
 }
