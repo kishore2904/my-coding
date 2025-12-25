bab5f7064f6981c76435c9c11bca91d6f789fa4f
kishore2904 <kishore290499@gmail.com>
2025-12-25 11:49:51 +0530

    dummy commit

--- src/Practice/Beginner.java
+++ src/Practice/Beginner.java
@@ -1,4 +1,4 @@
-package Practice.Beginner;
+package Practice;
 
 import java.util.HashSet;
 import java.util.List;
---
+++ src/Practice/Intermediate.java
@@ -0,0 +1,19 @@
+package Practice;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Intermediate {
+    public static void main(String[] args){
+        //11. Find the second-highest number in a list.
+        //List<Integer> numbers = List.of(5, 10, 5, 20, 10, 30);
+        //Integer first = numbers.stream().sorted(Comparator.reverseOrder()).distinct().skip(1).findFirst().orElseThrow();
+        //System.out.println("second-highest number in a list: "+first);
+
+        //12. Find the second-lowest number in a list.
+        //List<Integer> numbers = List.of(5, 10, 5, 20, 10, 30);
+        //Integer first = numbers.stream().sorted().distinct().skip(1).findFirst().orElseThrow();
+        //System.out.println("second-lowest number in a list: "+first);
+
+    }
+}
