4e812436adac689abb181b267e05dd1a126c25a6
unfold <studiovogue01@gmail.com>
2025-12-25 11:30:14 +0530

    Beginner Level 7-10 completed

--- src/Practice/Beginner/Beginner.java
+++ src/Practice/Beginner/Beginner.java
@@ -2,6 +2,7 @@ package Practice.Beginner;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -28,6 +29,27 @@ public class Beginner {
         //numbers = [5, 10, 5, 20, 10, 30];
         //Set<Integer> collect = Stream.of(5, 10, 5, 20, 10, 30).collect(Collectors.toSet());
         //System.out.println(collect);
+
+        //7. Find maximum value in a list
+        //<Integer> numbers = List.of(5, 10, 5, 20, 10, 30);
+        //Integer i = numbers.stream().max(Integer::compareTo).get();
+        //System.out.println("Maximum value in the list is: "+i);
+
+        //8. Find minimum value in a list
+        //List<Integer> numbers = List.of(5, 10, 5, 20, 10, 30);
+        //Integer i = numbers.stream().min((o1, o2) -> o1 - o2).get();
+        //System.out.println("Minimum value in the list is: "+i);
+
+        //9. Find sum of all numbers in a list
+        //List<Integer> numbers = List.of(5, 10, 5, 20, 10, 30);
+        //Integer sum = numbers.stream().reduce(Integer::sum).get();
+        //System.out.println("Sum of all the numbers in given list is: "+sum);
+
+        //10. Join list of strings into a single string
+        //List<String> stringList = List.of("This","is","a");
+        //String join = String.join("", stringList); (or) stringList.stream().collect(Collectors.joining())
+        //System.out.println(join);
+
 
     }
 }
