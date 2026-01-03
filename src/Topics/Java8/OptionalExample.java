03cdae408c81ec9f52ef4ee14b56282d673d3bf4
kishore2904 <kishore290499@gmail.com>
2026-01-03 22:55:51 +0530

    OptionalExample

--- src/Topics/Java8/OptionalExample.java
+++ src/Topics/Java8/OptionalExample.java
@@ -5,11 +5,14 @@ import java.util.*;
 public class OptionalExample {
     public static void main(String[] args){
 
-        String name = null;
-        Optional<String> optionalString = Optional.ofNullable(name);
+        //String name = null;
+        //Optional<String> optionalString = Optional.ofNullable(name);
 
 //        String result = optionalString.orElse("NO Name");
 //        System.out.println(result);
+
+        Integer sum = List.of(1,2,3,4,5).stream().reduce((integer, integer2) -> integer2-integer).get();
+        System.out.println(sum);
 
 
 
